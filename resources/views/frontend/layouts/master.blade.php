0328dd24e0ee9d3d06f5fea028b538020a295ab4
shubham <shubhamchauhan609060@gmail.com>
2023-05-17 16:53:45 +0530

    set favicon

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -15,7 +15,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="{{asset('frontend/images/icons/ffu_favicon_main.png')}}"/>
+	<link rel="icon" type="image/png" href="{{asset('frontend/images/icons/favicon.png')}}"/>
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{asset('frontend/vendor/bootstrap/css/bootstrap.min.css')}}"/>
 <!--===============================================================================================-->
